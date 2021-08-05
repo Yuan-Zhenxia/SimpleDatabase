04d723c3182313ec514f88a39b2cc0f50453971d
龙哥 <d>
2021-08-05 15:05:48 +0800

    first

--- src/main/java/simpleDatabase/operator/Aggregate.java
+++ src/main/java/simpleDatabase/operator/Aggregate.java
@@ -3,6 +3,7 @@ package simpleDatabase.operator;
 import java.util.NoSuchElementException;
 
 /**
+ *
  * The Aggregation operator that computes an aggregate (e.g., sum, avg, max,
  * min). Note that we only support aggregates over a single column, grouped by a
  * single column.
